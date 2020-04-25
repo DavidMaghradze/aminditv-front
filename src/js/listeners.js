8ad59894771f2fa213d2a3a45fc7ef90910a9e41
David Maghradze <davidmaghradze07@gmail.com>
2020-04-26 00:56:49 +0400

    added size changes in home page and update last issues

--- src/js/listeners.js
+++ src/js/listeners.js
@@ -27,6 +27,12 @@ $(document).ready(function() {
     const geoDropdown = $(".dropdown--geo")
     const isVisible = geoDropdown.hasClass("visible")
 
+    const filterSimpleSelects = $(".filters .select--simple").find(".dropdown")
+    console.log("filterSimpleSelects", filterSimpleSelects)
+    filterSimpleSelects.each(function() {
+      $(this).removeClass("visible")
+    })
+
     if (isVisible) {
       geoDropdown.removeClass("visible")
       setTimeout(() => {
@@ -37,12 +43,34 @@ $(document).ready(function() {
     }
   })
 
-  $(".select").click(function(event) {
+  $(".select--simple").click(function(event) {
     event.stopPropagation()
-    console.log($(this).find(".dropdown"))
+    const cityDropdowns = $(".dropdown--cities")
+    let isVisible = ""
+
+    isVisible = cityDropdowns.map(function() {
+      if ($(this).hasClass("visible")) {
+        return $(this)
+      }
+    })
+
+    if (isVisible.length !== 0) {
+      isVisible[0].removeClass("visible")
+    }
+
     $(this)
       .find(".dropdown")
       .toggleClass("visible")
+  })
+
+  // change header background on scroll
+  $(window).scroll(function() {
+    const scrollTop = $(window).scrollTop()
+    if (scrollTop > 0) {
+      $(".header").addClass("scrolled")
+    } else {
+      $(".header").removeClass("scrolled")
+    }
   })
 
   // show-hide searchbar on mobile
@@ -62,5 +90,14 @@ $(document).ready(function() {
     $(".header__nav-mobile__header").click(function() {
       $(".header__nav-mobile").removeClass("shown")
     })
+  })
+
+  $(".header__nav-item--dropdown").click(function() {
+    $(this)
+      .find("span")
+      .toggleClass("shown")
+    $(this)
+      .next()
+      .slideToggle("fast")
   })
 })
